28ee497fda6c6bb2b349b7946c9e70310d921fe7
Avinash Choudhary <55015213+Avi-nash5@users.noreply.github.com>
2019-10-09 10:45:41 +0530

    Add files via upload

--- Codechef/Oct_long/Matrix.cpp
+++ Codechef/Oct_long/Matrix.cpp
@@ -1,38 +1,35 @@
 #include<bits/stdc++.h>
 using namespace std;
 
-vector< vector< int>> update_mat(vector< vector< int>> mat, pair<int,int> point, int N, int M){
-	for(int i=0,j=0; i<M,j<N; i++,j++){
-		mat[point.first -1][i] += 1;
-		mat[j][point.second -1] += 1;
-	}
-	return mat;
-}
+
+
 int main(){
 	int T;
 	cin>>T;
 	for(int i=0; i<T; i++){
-		int N,M,Q,ans=0;
+		long long int N,M,Q,x,y,countr=0,countc=0,ans;
 		cin>>N>>M>>Q;
-		vector< vector< int>> mat(N);
-		for(int j=0; j<N; j++){
-			mat[j] = vector<int>(M,0);
-		}
-		pair<int,int> point;
-		for(int j=0; j<Q; j++){
-			cin>>point.first;
-			cin>>point.second;
+		vector<long long int> R(N,0), C(M,0);
 
-			mat = update_mat(mat, point, N, M);
+		for(int j=0; j<Q; j++){
+			cin>>x>>y;
+			x--;
+			y--;
+			R[x]++;
+			C[y]++;
 		}
 
 		for(int i=0; i<N; i++){
-			for(int j=0; j<M; j++){
-				cout<<mat[i][j]<<" ";
-				if(mat[i][j]%2 != 0)
-					ans++;
-			}
+			if(R[i]%2==1)
+				countr++;
 		}
+
+		for(int i=0; i<M; i++){
+			if(C[i]%2==1)
+				countc++;
+		}
+
+		ans = countr*M + countc*N -2*countr*countc;
 		cout<<ans<<endl;
 	}
 
