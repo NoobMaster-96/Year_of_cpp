c487e14ac9b0d5d57bd999de11ce4c03e0382649
Avinash Choudhary <55015213+Avi-nash5@users.noreply.github.com>
2020-08-26 12:44:01 +0530

    Add files via upload

---
+++ Hackercup/ProblemA.cpp
@@ -0,0 +1,13 @@
+#include<bits/stdc++.h>
+using namespace std;
+#define endl "\n"
+
+int main(){
+    cout<<setfill('$');
+    cout<<setw(sizeof(int))<<9;
+    cout<<setfill('#');
+    cout<<left<<setw(sizeof(char))<<4;
+    cout<<setfill('@');
+    cout<<right<<setw(sizeof(double))<<18;
+    
+}
