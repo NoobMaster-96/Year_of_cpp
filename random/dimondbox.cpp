c8974efdb28228efb1b1a58a5527554e1a12ec82
Avinash Choudhary <avinash.ee61@gmail.com>
2020-12-01 01:17:07 +0530

    01:16/01-12-2020

--- random/dimondbox.cpp
+++ random/dimondbox.cpp
@@ -1,32 +1,52 @@
-#include<bits/stdc++.h>
+#include <bits/stdc++.h>
 using namespace std;
+#define ll long long
+#define pb push_back
 
-bool maxsum(vector<vector<int>>& a, int r, int previdx, int& sum){
-	int i=0;
-	bool flag = false;
-	while(i<7){
-		if(i!=previdx){
-
-		}
-	}
+ll nCr(ll n, ll k){
+    if(n==0 && k==0) return 0;
+    ll res = 1;
+    if ( k > n - k )
+        k = n - k;
+    for (ll i = 0; i < k; ++i){
+        res *= (n - i);
+        res /= (i + 1);
+    }
+    return res;
 }
-int main(){
-	int t;
-	cin>>t;
-	while(t--){
-		int n,m;
-		cin>>n>>m;
-		vector<vector<int>> a(n,vector<int>(7,0));
-		for(int i=0;i<m;i++){
-			int b,c,v;
-			cin>>b>>c>>v;
-			a[b-1][c-1] = max(a[b-1][c-1],v);
-		}
-		for(int i=0;i<n;i++){
-			for(int j=0;j<7;j++){
-				cout<<a[i][j]<<" ";
-			}
-			cout<<endl;
-		}
-	}
+
+int main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL); cout.tie(NULL);
+
+    int t = 1;
+    cin>>t;
+    ll x, y, k;
+    while(t--){
+        cin>>x>>y>>k;
+        while(1){
+            if(x==0 && y==0) break;
+            else if(x==0){
+                cout<<"V";
+                y--;
+            }
+            else if(y==0){
+                cout<<"H";
+                x--;
+            }
+            else {
+                if(nCr(x+y-1, x-1) > k){
+                    x--;
+                    cout<<"H";
+                }
+                else {
+                    k = k - nCr(x+y-1, x-1);
+                    y--;
+                    cout<<"V";
+                }
+            }
+        }
+        cout<<"\n";
+    }
+    return 0;
 }
